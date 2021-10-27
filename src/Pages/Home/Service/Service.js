d00c49509e75ecc30ad5dd19025cbbf68706da27
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2021-10-27 19:52:09 +0600

    random background added

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -12,11 +12,12 @@ const Service = (props) => {
     const handleEventClick = () => {
         history.push(`/services/${_id}`);
     }
+    const randomColor = "#" + (Math.floor(Math.random() * 16777215).toString(16));
     return (
         <div>
             <Col>
-                <Card onClick={handleEventClick} className='shadow h-100 service-card rounded'>
-                    <Card.Img className='mx-auto service-img' variant="top" src={img} />
+                <Card onClick={handleEventClick} className='shadow h-100 service-card rounded-card' style={{backgroundColor:randomColor}}>
+                    <Card.Img className='img-fluid' variant="top" src={img} />
                     <Card.Body>
                         <Card.Title className='text-center'>{title}</Card.Title>
                     </Card.Body>
