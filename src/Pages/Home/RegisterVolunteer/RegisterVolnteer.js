4d4a178bfc6a8e87cb6491fdb58d85ee382f0e95
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2021-10-27 13:35:25 +0600

    register as volnteer half work done

--- src/Pages/Home/RegisterVolunteer/RegisterVolnteer.js
+++ src/Pages/Home/RegisterVolunteer/RegisterVolnteer.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { useParams } from 'react-router';
+import useAuth from '../../../Hooks/useAuth';
+import './RegisterVolunteer.css';
 
 const RegisterVolnteer = () => {
     const [services, setServices] = useState([]);
@@ -9,9 +12,49 @@ const RegisterVolnteer = () => {
             .then(res => res.json())
             .then(data => setServices(data));
     }, [])
+
+
+
+
+
+
+
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+    const { user } = useAuth();
+    const onSubmit = data => {
+        fetch('http://localhost:5000/regUser', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(result => {
+                if (result.insertedId) {
+                    alert('order processed successfully');
+                    reset();
+                }
+            })
+    };
+
+
     return (
         <div>
-            <h2>This is register volunteer: {services.title}</h2>
+            <h2 className='text-center mt-5'>Register Ss a volunteer</h2>
+            <div className='d-flex justify-content-center align-items-center'>
+                <form className="register-form" onSubmit={handleSubmit(onSubmit)}>
+                    <input defaultValue={user.displayName} {...register("name")} />
+                    <input defaultValue={user.email} {...register("email", { required: true })} />
+                    {errors.email && <span className="error">This field is required</span>}
+                    <input defaultValue={services.title} {...register("Servive name")} />
+                    <input placeholder="Address" defaultValue="" {...register("address")} />
+                    <input placeholder="City" defaultValue="" {...register("city")} />
+                    <input placeholder="phone number" defaultValue="" {...register("phone")} />
+
+                    <input type="submit" />
+                </form>
+            </div>
         </div>
     );
 };
