237fcfac8ef210db907029b1a603cf5df60014f8
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2021-10-28 10:16:29 +0600

    user delete

--- src/Pages/Home/AllUser/AllUser.js
+++ src/Pages/Home/AllUser/AllUser.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const AllUser = (props) => {
-    const { service,name,email,date } = props.user;
+    const [myEvents, setMyEvents] = useState([]);
+    useEffect(() => {
+        fetch('https://infinite-fortress-64339.herokuapp.com/myEvent')
+            .then(res => res.json())
+            .then(data => setMyEvents(data))
+    }, []);
+
+
+    const handleDelete = (id) => {
+        const proceed = window.confirm('Are you sure, you want to delete?');
+        if (proceed) {
+            const url = `https://infinite-fortress-64339.herokuapp.com/myEvent/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        alert('deleted successfully');
+                        const remainingVolunteers = myEvents.filter(volunteer => volunteer._id !== id);
+                        setMyEvents(remainingVolunteers);
+                        // window.reload();
+                        window.location.reload();
+                    }
+                });
+        }
+    }
+
+
+    const { _id,service,name,email,date } = props.user;
     return (
         <div>
             <table className="table">
@@ -11,7 +40,7 @@ const AllUser = (props) => {
                         <td>{email}</td>
                         <td>{date}</td>
                         <td>{service}</td>
-                        <td><button className='btn btn-secondary'>Delete</button></td>
+                        <td><button onClick={()=>handleDelete(_id)} className='btn btn-secondary'>Delete</button></td>
                     </tr>
                 </tbody>
             </table>
